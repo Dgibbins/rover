a78bbca0ff306c50268ada04187f33eccdb9c3d8
Dgibbins <nayr_ryan@hotmail.com>
2017-01-18 18:32:42 -0500

    Rover1 Rough layout

--- rover.rb
+++ rover.rb
@@ -15,8 +15,8 @@
 # need to figure out from here is how to read and split up the instructions so that you
 # can pass them on to your rovers.
 
-max_x=0
-max_y=0
+$max_x=5
+$max_y=5
 
 class Rover
 attr_reader :x, :y, :direction
@@ -27,58 +27,99 @@ def initialize(x_position, y_position, direction)
   @direction = direction
 end
 
-# puts "Define the area of your plateau:"
-# @plateau=gets.chomp
-#
-# puts "Enter your x,y and heading for Rover 1:"
-# @rover1_position=gets.chomp
-#
-# puts "Enter your instructions for Rover 1 (L/R/M-Left/Right/Move):"
-# @rover1_instructions=gets.chomp
-#
-# puts "Enter your x,y and heading for Rover 2:"
-# @rover2_position=gets.chomp
-#
-# puts "Enter your instructions for Rover 2 (L/R/M-Left/Right/Move):"
-# @rover2_instructions=gets.chomp
-
 
   def read_instruction(rover_instructions)
+      rover_instructions.each do
 
-    if l || r
-      turn
-    end
+        if 'l' || 'r'
+          turn
+        end
 
-    if m
-      move
-    end
+        if 'm'
+          move
+        end
 
+      end
+        return "Position is #{@x_position}, #{@y_position}, #{@direction}"
   end
 
 
 
   def move
 
-    when North
+    when 'N'
       @y_position+=1
     end
 
-    when East
-      @x_position+=1
-    end
+      when 'E'
+        @x_position+=1
+      end
 
-    when South
-      @y_position-=1
-    end
+        when 'S'
+          @y_position-=1
+        end
 
-    when West
-      @x_position-=1
-    end
-
+          when 'W'
+            @x_position-=1
+          end
   end
 
-def turn
-  if left
-    @heading
+  def turn
+
+    when 'N'
+      if 'l'
+         @direction= 'W'
+        else
+         @direction= 'E'
+      end
+    end
+
+    when 'E'
+      if l
+         @direction= 'N'
+        else
+         @direction= 'S'
+      end
+    end
+
+    when 'S'
+      if 'l'
+         @direction= 'E'
+       else
+         @direction= 'W'
+      end
+    end
+
+      when 'W'
+        if 'l'
+           @direction = 'S'
+         else
+           @direction= 'W'
+        end
+      end
   end
-end
+
+  puts "Define the area of your plateau:"
+  plateau=gets.chomp
+
+  puts "Enter your x,y and heading for Rover 1:"
+  rover1_position=gets.chomp
+
+  puts "Enter your instructions for Rover 1 (L/R/M-Left/Right/Move):"
+  rover1_instructions=gets.chomp
+
+  # puts "Enter your x,y and heading for Rover 2:"
+  # rover2_position=gets.chomp
+  #
+  # puts "Enter your instructions for Rover 2 (L/R/M-Left/Right/Move):"
+  # rover2_instructions=gets.chomp
+
+  data_position = rover1_position.split(" ")
+
+  data_instructions= rover1_instructions.split(" ")
+
+  rover_instructions = data_instructions.downcase
+
+  rover1=Rover.new(data[0],data[1],data[2])
+
+  rover1.read_instruction(rover_instructions)
